6c7ac954d11ef9e33606b60f0334f9d95db5ed58
FedericoPosada2 <fedeposa122@gmail.com>
2020-02-25 15:11:28 -0500

    Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_195229) do
+ActiveRecord::Schema.define(version: 2020_02_25_194951) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
-    t.date "released_at"
+    t.string "released_at"
     t.integer "artist_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
